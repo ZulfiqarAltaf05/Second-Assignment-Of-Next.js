0e07aa0ecde6ada3f71c6a3a2002fc213cae9e19
ZulfiqarAltaf05 <zulfiqaraltaf999@gmail.com>
2024-10-30 05:12:47 -0700

    first commit

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+const About = () => {
+    return (
+        <div>
+            <h1>About Page</h1><br /><hr />
+            <Link href="/">Go to Homepage</Link>
+        </div>
+    );
+};
+
+export default About
---
+++ app/header/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const Header = () =>{
+    return (
+        <div>
+            <h1>Header Page</h1>
+            <br />
+            <hr />
+            <Link href="/">Go To Home Page</Link>
+        </div>
+    )
+}
+
+export default Header
